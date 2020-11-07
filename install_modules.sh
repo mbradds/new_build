5ff59899a80745ad8e7bbcdcf8fc8527831e72e5
mbradds <gmoss@ualberta.ca>
2020-11-07 13:30:50 -0700

    figured out how to run files

--- install_modules.sh
+++ install_modules.sh
@@ -2,28 +2,32 @@
 # A script to install my software on a clean ubuntu install
 
 #get user input
-#echo 'Ask for confirmation for each module? (y/n)'
-#read eachModule
-#echo $eachModule
+read -p "sudo apt-get update? (y/n) " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "run update"
+    sudo apt-get update
+fi
 
-declare -a anaconda=("Anaconda" "module1 command1 && module1 command2")
-declare -a nodejs=("nodejs" "module2 command1 && module2 command2")
-declare -a packages=("anaconda" "nodejs")
+
+declare -a packages=("nodejs" "nmp" "qgis" "code" "mssql-server" "azuredatastudio")
+rm -f ./install_logs/already_installed.txt #force remove anything in the already installed file
+rm -f ./install_logs/installed.txt #force remove anything in the already installed file
+
 
 for package in "${packages[@]}"; do
-    declare -n lst=$package
-    package_name=${lst[0]}
-    if [ $(dpkg-query -W -f='${Status}' ${package_name} 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-        echo "$package_name is not installed. Starting install";
+    if [ $(dpkg-query -W -f='${Status}' ${package} 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+        echo "$package is not installed. Starting install...";
+        sh ./${package}.sh
+        echo $(date -u) $package >> 'install_logs/installed.txt'
     else
-        echo "$package_name is already installed"
+        install_status="$package already installed"
+        echo $install_status
+        echo $(date -u) $install_status >> 'install_logs/already_installed.txt'
     fi
-    # cmd="${lst[1]} &> ${package_name}.log"
-    # echo "Starting ${package_name}..."
-    # echo ${cmd}
-    # echo "Completed ${package_name}..."
-
 done
+
 
 
 #write to a file
