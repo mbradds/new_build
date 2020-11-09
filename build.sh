82c79748379c35a8fbd0d03ee932781a26e395a8
mbradds <gmoss@ualberta.ca>
2020-11-08 18:57:02 -0700

    cleaned up log files

--- build.sh
+++ build.sh
@@ -22,24 +22,21 @@ function askUserAboutPackage(){
     then
         $cmdpath
         echo $(date -u) $pkg >> $logpath
-        sudo apt-get update
+        apt-get update
     else
         echo $(date -u) $pkg >> $logpathno
     fi
 }
 
-
 declare -a packages=("nodejs" "npm" "git" "qgis" "code" "mssql-server" "sqlite3")
-#force remove any content in the log files
-mkdir -p install_logs
-rm -f ./install_logs/already_installed.txt 
-rm -f ./install_logs/installed.txt 
-rm -f ./install_logs/skipped_install.txt 
-rm -f ./install_logs/uninstalled.txt 
-rm -f ./install_logs/skipped_uninstall.txt 
+declare -a log_files=("already_installed" "installed" "skipped_install" "uninstalled" "skipped_uninstall")
+mkdir -p install_logs 
 
+for log in "${log_files[@]}"; do #force remove any content in the log files
+    rm -f ./install_logs/${log}.txt
+done
 
-sudo apt-get update
+apt-get update && apt-get dist-upgrade
 for package in "${packages[@]}"; do
     if [ $(dpkg-query -W -f='${Status}' ${package} 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         askUserAboutPackage "$package is not installed. Start install? (y/n)" "${package}" "y"
@@ -48,7 +45,7 @@ for package in "${packages[@]}"; do
     fi
 done
 
-
-#"Anaconda doesnt appear in default list of packages"
+#Anaconda doesnt appear in default list of packages
 askUserAboutPackage "Cant verify if Anaconda is installed. Uninstall? (y/n)" "Anaconda" "y"
 askUserAboutPackage "Install Anaconda? (y/n)" "Anaconda" "n"
+apt-get dist-upgrade
